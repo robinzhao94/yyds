74aafd7a738aca3a9ca5baa6b565a419a696a43b
zhaopeng <peng.zhao@rootcloud.com>
2021-11-02 22:41:19 +0800

    应用大神-项目初始化

---
+++ yyds-common/src/main/java/com/rootcloud/yyds/common/YydsCommonApplication.java
@@ -0,0 +1,13 @@
+package com.rootcloud.yyds.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YydsCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YydsCommonApplication.class, args);
+    }
+
+}
---
+++ yyds-gateway/src/test/java/com/rootcloud/yyds/gateway/YydsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootcloud.yyds.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YydsGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
